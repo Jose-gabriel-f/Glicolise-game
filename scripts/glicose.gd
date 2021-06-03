ddddba1d2712e8b257cc256f5fdf479406a5fd68
José Gabriel <62730379+Jose-gabriel-f@users.noreply.github.com>
2021-06-02 22:48:38 -0300

    Update glicose.gd

--- scripts/glicose.gd
+++ scripts/glicose.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var selecionado = false
+var dentro_celula = false
 
 func _ready():
 	pass 
@@ -14,12 +15,13 @@ func followMouse():
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-		if event.pressed == true:
+		if event.pressed == true and dentro_celula == false:
 			selecionado = true
 		else:
 			selecionado = false
 			
 func _on_Area2D_area_entered(area):
 	get_node("AnimationPlayer").play("animacaoglicose")
+	dentro_celula = true
 	
 	
