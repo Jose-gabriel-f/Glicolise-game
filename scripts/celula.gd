dec64d3a53728ef8043e8f3725df8271ede61ea4
José Gabriel <62730379+Jose-gabriel-f@users.noreply.github.com>
2021-07-14 16:51:00 -0300

    adding change scenes

--- scripts/celula.gd
+++ scripts/celula.gd
@@ -2,6 +2,12 @@ extends Node2D
 # var tipo_reagente --> fosfato, ATP
 func _ready():
 	pass
-
+	
 func _on_Area2D_area_entered(area):
 	print(area.get_name(), " entrou na célula")
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		if event.position.x > 424 and event.position.x < 575 and event.position.y > 123 and event.position.y < 275:
+			print("click", event.position)
+			get_tree().change_scene("res://scenes/glicolise.tscn")
