808cdbde4632599302da6649642bbf615ba93a2d
José Gabriel <jgabrieldeflores@gmail.com>
2021-11-19 13:46:30 -0300

    Adição de novo objetivo na lista

--- scripts/Button.gd
+++ scripts/Button.gd
@@ -7,6 +7,7 @@ func _on_Button_pressed():
 	if $"../CameraGeral".current:
 		$"../celula/CameraDetalhe".current = true
 		$AnimationPlayer.play("ida")
+		$"../Control/CheckBox3".pressed = true
 	else :
 		$"../CameraGeral".current = true
 		$AnimationPlayer.play("volta")
